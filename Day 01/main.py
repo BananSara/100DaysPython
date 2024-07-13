d588507271732d7ca37510f9058b894a5c81f590
sara <sarabanann@gmail.com>
2024-07-13 18:43:27 +0330

    brand name program

--- Day 01/main.py
+++ Day 01/main.py
@@ -1,1 +1,7 @@
 print('hello world!')
+# #####################
+
+city_name = input('whats is name of the city you grew up in?')
+pet = input('what is your pet name?')
+
+print('your brand name is: ' + city_name + ' ' + pet)
